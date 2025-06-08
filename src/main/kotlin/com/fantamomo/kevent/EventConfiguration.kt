54a2a63597be1d43f9ab1d276624a1375155d0a0
Fantamomo <git@fantamomo.com>
2025-06-08 17:22:49 +0200

    feat: add `EventConfiguration` class
    - Introduced `EventConfiguration` for managing key-value pairs with generics.
    - Added support for `get`, `getOrDefault`, and `getValue` operations.
    - Included constructor integration with `EventConfigurationScope`.

---
+++ src/main/kotlin/com/fantamomo/kevent/EventConfiguration.kt
@@ -0,0 +1,14 @@
+package com.fantamomo.kevent
+
+class EventConfiguration(private val data: Map<Key<*>, Any?>) {
+    constructor(scope: EventConfigurationScope) : this(scope.data.toMap())
+
+    @Suppress("UNCHECKED_CAST")
+    operator fun <T> get(key: Key<T>) = data[key] as? T
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T> getOrDefault(key: Key<T>) = data[key] as? T ?: key.defaultValue
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T : Any> getValue(key: Key<T>): T = data[key] as T
+}
