18974b4f30a2af53a4adb8d83a65bcdab7dd1ff7
Fantamomo <git@fantamomo.com>
2025-06-08 14:26:44 +0200

    feat: enhance `Priority` creation logic
    - Added `Standard` companion object to map priorities to predefined levels.
    - Updated `Priority` invocation to return `Standard` or `Custom` based on input.

--- src/main/kotlin/com/fantamomo/kevent/Priority.kt
+++ src/main/kotlin/com/fantamomo/kevent/Priority.kt
@@ -9,11 +9,17 @@ sealed interface Priority {
         NORMAL(0),
         LOW(-500),
         LOWEST(1000);
+
+        companion object {
+            private val BY_PRIORITY = entries.associateBy(Standard::priority)
+
+            fun fromPriority(priority: Int) = BY_PRIORITY[priority]
+        }
     }
 
     data class Custom(override val priority: Int) : Priority
 
     companion object {
-        operator fun invoke(priority: Int) = Custom(priority)
+        operator fun invoke(priority: Int): Priority = Standard.fromPriority(priority) ?: Custom(priority)
     }
 }
