5ff45126eed1b5424539f4dda341dfc972651f43
Fantamomo <git@fantamomo.com>
2025-06-09 19:31:55 +0200

    feat: add `DISALLOW_SUBTYPES` built-in key to `Key` class
    - Introduced `DISALLOW_SUBTYPES` as a built-in key for specifying whether event listeners should disallow all subtypes of an event.
    - Updated KDoc to reflect the new key and its usage within the event configuration system.

--- src/main/kotlin/com/fantamomo/kevent/Key.kt
+++ src/main/kotlin/com/fantamomo/kevent/Key.kt
@@ -4,14 +4,14 @@ import kotlin.reflect.KClass
 
 /**
  * Type-safe key for storing and retrieving configuration options.
- * 
+ *
  * The [Key] class is a central part of the event configuration system. It provides
  * a type-safe way to store and retrieve configuration options of any type. Each key
  * has a string identifier, a Kotlin class type, and a default value.
- * 
+ *
  * The event system includes built-in keys like [PRIORITY], but third-party systems
  * can define their own keys to store custom configuration options.
- * 
+ *
  * Example of defining a custom key:
  * ```
  * // In a third-party library
@@ -20,7 +20,7 @@ import kotlin.reflect.KClass
  *     val MY_FLAG = Key("myFlag", false)
  * }
  * ```
- * 
+ *
  * Example of using a custom key:
  * ```
  * @Register
@@ -28,19 +28,20 @@ import kotlin.reflect.KClass
  *     configuration(event) {
  *         // Built-in option
  *         priority = Priority.HIGH
- *         
+ *         disallowSubtypes = true
+ *
  *         // Custom option
  *         set(MyKeys.MY_OPTION, "custom value")
  *         set(MyKeys.MY_FLAG, true)
  *     }
  * }
  * ```
- * 
+ *
  * @param T The type of value associated with this key
  * @property key The string identifier for this key
  * @property type The Kotlin class representing the type T
  * @property defaultValue The default value to use if no value is set
- * 
+ *
  * @see EventConfigurationScope
  *
  * @author Fantamomo
@@ -50,32 +51,33 @@ data class Key<T>(val key: String, val type: KClass<T & Any>, val defaultValue: 
     companion object {
         /**
          * Built-in key for setting the priority of an event handler.
-         * 
+         *
          * This key is used with the [priority] extension property on [EventConfigurationScope].
-         * 
-         * Example:
-         * ```
-         * configuration(event) {
-         *     priority = Priority.HIGH
-         * }
-         * ```
-         * 
-         * @see com.fantamomo.kevent.priority
+         *
+         * @see priority
          */
         val PRIORITY = Key<Priority>("priority", Priority.Standard.NORMAL)
+        /**
+         * Build-in key for defining if the listener disallow all subtypes of an event.
+         *
+         * This key is uses with the [disallowSubtypes] extension property on [EventConfigurationScope].
+         *
+         * @see disallowSubtypes
+         */
+        val DISALLOW_SUBTYPES = Key<Boolean>("disallowSubtypes", false)
 
         /**
          * Convenience factory method for creating keys with reified types.
-         * 
+         *
          * This method simplifies the creation of keys by inferring the type from
          * the reified type parameter, eliminating the need to explicitly specify
          * the class.
-         * 
+         *
          * Example:
          * ```
          * val MY_KEY = Key("myKey", "default value")
          * ```
-         * 
+         *
          * @param key The string identifier for the key
          * @param defaultValue The default value to use if no value is set
          * @return A new Key instance with the specified parameters
