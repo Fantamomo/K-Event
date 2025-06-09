657c51e26adea78d1f2dc617faed9269d7b362e0
Fantamomo <git@fantamomo.com>
2025-06-09 10:08:14 +0200

    feat: add type parameter to `EventConfiguration`
    - Introduced generic type parameter `<E : Event>` to `EventConfiguration` for improved type safety.
    - Updated `DEFAULT` companion object to use `EventConfiguration<Nothing>`.

--- src/main/kotlin/com/fantamomo/kevent/EventConfiguration.kt
+++ src/main/kotlin/com/fantamomo/kevent/EventConfiguration.kt
@@ -1,7 +1,7 @@
 package com.fantamomo.kevent
 
-class EventConfiguration(private val data: Map<Key<*>, Any?>) {
-    constructor(scope: EventConfigurationScope) : this(scope.data.toMap())
+class EventConfiguration<E : Event>(private val data: Map<Key<*>, Any?>) {
+    constructor(scope: EventConfigurationScope<E>) : this(scope.data.toMap())
 
     @Suppress("UNCHECKED_CAST")
     operator fun <T> get(key: Key<T>) = data[key] as? T
@@ -13,6 +13,6 @@ class EventConfiguration(private val data: Map<Key<*>, Any?>) {
     fun <T : Any> getValue(key: Key<T>): T = data[key] as T
 
     companion object {
-        val DEFAULT = EventConfiguration(emptyMap())
+        val DEFAULT = EventConfiguration<Nothing>(emptyMap())
     }
 }
