b285ed11d914c128367b40afefdb7dc9a6f67cfa
Fantamomo <git@fantamomo.com>
2025-06-09 11:53:34 +0200

    docs: add KDoc for `getOrDefault` utility function

--- src/main/kotlin/com/fantamomo/kevent/util.kt
+++ src/main/kotlin/com/fantamomo/kevent/util.kt
@@ -1,3 +1,21 @@
 package com.fantamomo.kevent
 
+/**
+ * Utility extension function to get a configuration value or its default.
+ * 
+ * This function retrieves the value associated with the specified key from the
+ * configuration scope, or returns the key's default value if no value is set.
+ * 
+ * It's used internally by other parts of the event system, such as the [priority]
+ * extension property, to provide default values for configuration options.
+ * 
+ * @param key The key to look up
+ * @return The value associated with the key, or the default value if not found
+ * 
+ * @see EventConfigurationScope.get
+ * @see Key.defaultValue
+ *
+ * @author Fantamomo
+ * @since 1.0-SNAPSHOT
+ */
 fun <E : Event, T> EventConfigurationScope<E>.getOrDefault(key: Key<T>): T = get(key) ?: key.defaultValue
