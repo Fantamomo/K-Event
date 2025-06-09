78a709dfce49cd9c076580a28da1bf19dcf1e373
Fantamomo <git@fantamomo.com>
2025-06-09 10:16:27 +0200

    feat: enhance `Key` class with companion object and generic invoke operator
    - Added `PRIORITY` as a predefined `Key` instance.
    - Introduced a generic `invoke` operator for simplified `Key` creation.

--- src/main/kotlin/com/fantamomo/kevent/Key.kt
+++ src/main/kotlin/com/fantamomo/kevent/Key.kt
@@ -2,4 +2,12 @@ package com.fantamomo.kevent
 
 import kotlin.reflect.KClass
 
-data class Key<T>(val key: String, val type: KClass<T & Any>, val defaultValue: T)
+data class Key<T>(val key: String, val type: KClass<T & Any>, val defaultValue: T) {
+    companion object {
+        val PRIORITY = Key<Priority>("priority", Priority.Standard.NORMAL)
+
+        @Suppress("UNCHECKED_CAST")
+        inline operator fun <reified T> invoke(key: String, defaultValue: T): Key<T> =
+            Key(key, T::class as KClass<T & Any>, defaultValue)
+    }
+}
