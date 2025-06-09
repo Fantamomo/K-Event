da0009a9b84299a0ced2c419c5d4f4e8861a7e22
Fantamomo <git@fantamomo.com>
2025-06-09 11:52:50 +0200

    docs: add KDoc for `priority` property in `EventConfigurationScope`
    - Documented the purpose and usage of the `priority` property.
    - Included an example demonstrating how to set and retrieve priorities.
    - Provided references to `Priority` and `Key.PRIORITY` for further context.

--- src/main/kotlin/com/fantamomo/kevent/options.kt
+++ src/main/kotlin/com/fantamomo/kevent/options.kt
@@ -1,5 +1,30 @@
 package com.fantamomo.kevent
 
+/**
+ * Extension property for setting and getting the priority of an event handler.
+ * 
+ * This property provides a convenient way to access the [Priority] configuration
+ * option in the event configuration DSL. It uses the predefined [Key.PRIORITY] key
+ * to store and retrieve the priority value.
+ * 
+ * Example usage:
+ * ```
+ * @Register
+ * fun onMyEvent(event: MyEvent?) {
+ *     configuration(event) {
+ *         priority = Priority.HIGH
+ *     }
+ * }
+ * ```
+ * 
+ * The default priority is [Priority.Standard.NORMAL] if not explicitly set.
+ * 
+ * @see Priority
+ * @see Key.PRIORITY
+ *
+ * @author Fantamomo
+ * @since 1.0-SNAPSHOT
+ */
 var <E : Event> EventConfigurationScope<E>.priority: Priority
     get() = getOrDefault(Key.PRIORITY)
     set(value) = set(Key.PRIORITY, value)
