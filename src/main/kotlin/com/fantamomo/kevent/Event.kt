e963654bfcc57d4663de61ce896ce826588c8ac3
Fantamomo <git@fantamomo.com>
2025-06-10 09:01:17 +0200

    feat: add `name` property to `Event` class
    - Introduced `name` property derived from the event's simple class name for identification.
    - Added validation to ensure all events have a meaningful name, throwing `IllegalArgumentException` for unnamed classes.
    - Updated KDoc with details and usage of the new property.

--- src/main/kotlin/com/fantamomo/kevent/Event.kt
+++ src/main/kotlin/com/fantamomo/kevent/Event.kt
@@ -24,4 +24,16 @@ package com.fantamomo.kevent
  * @since 1.0-SNAPSHOT
  */
 open class Event {
+
+    /**
+     * The name of the event class derived from its simple class name.
+     *
+     * This property holds the simple name of the event class and is used as an identifier
+     * for event instances. If the class does not have a name (e.g., anonymous classes),
+     * an [IllegalArgumentException] is thrown. Ensures all events have a meaningful name
+     * for identification within the event system.
+     *
+     * @throws IllegalArgumentException if the event class does not have a name
+     */
+    val name: String = this::class.simpleName ?: throw IllegalArgumentException("Event class must have a name")
 }
