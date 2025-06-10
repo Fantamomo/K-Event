8a5216201d0f3c0bde64a6d8b5aac3d07fcd98e3
Fantamomo <git@fantamomo.com>
2025-06-10 09:18:51 +0200

    feat: add overloaded `register` method for custom event handling
    - Introduced a new `register` method enabling type-safe event handling with custom configuration and handler logic.
    - Added default `EventConfiguration` parameter for streamlined usage.

--- src/main/kotlin/com/fantamomo/kevent/EventManager.kt
+++ src/main/kotlin/com/fantamomo/kevent/EventManager.kt
@@ -1,7 +1,15 @@
 package com.fantamomo.kevent
 
+import kotlin.reflect.KClass
+
 interface EventManager {
     fun register(listener: Listener)
 
     fun dispatch(event: Event)
+
+    fun <E : Event> register(
+        event: KClass<E>,
+        configuration: EventConfiguration<E> = EventConfiguration.default(),
+        handler: (E) -> Unit
+    )
 }
