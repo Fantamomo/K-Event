2dc580ea7f84e91a9815d4d67dd33c20ad577f6e
Fantamomo <git@fantamomo.com>
2025-07-05 15:15:10 +0200

    feat: add `DeadEvent` to indicate undeliverable events
    - Introduced `DeadEvent` class to represent events with no registered listeners.
    - Provides debugging support for tracking unhandled events in the system.

---
+++ k-event-api/src/commonMain/kotlin/com/fantamomo/kevent/DeadEvent.kt
@@ -0,0 +1,21 @@
+package com.fantamomo.kevent
+
+/**
+ * Represents an event that could not be delivered to any registered listeners.
+ *
+ * The `DeadEvent` is used within the event system to signify that a particular event
+ * instance has no listeners or handlers capable of processing it. This helps in debugging
+ * or logging scenarios where it is desirable to track unhandled events in the system.
+ *
+ * A `DeadEvent` wraps the original event object that failed to be delivered, allowing
+ * the event system or application to inspect the original event properties or take
+ * other necessary actions.
+ *
+ * It is important to note that a `DeadEvent` is itself a valid event and can be processed
+ * by listeners that subscribe to it, if desired.
+ *
+ * @param event The original event instance that could not be delivered to any listeners
+ * @author Fantamomo
+ * @since 1.0-SNAPSHOT
+ */
+class DeadEvent(val event: Event) : Event()
