d1b1519f7b31c1c4709c288b87ae02a0a8a22fc5
Fantamomo <git@fantamomo.com>
2025-07-05 19:33:49 +0200

    feat(EventManager): add support for default parameter injection
    - Introduced `defaultParameterInjection` flag in `EventManager` factory methods.
    - Enhanced `DefaultEventManager` initialization to handle `defaultParameterInjection` configuration.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- k-event-manager/src/commonMain/kotlin/com/fantamomo/kevent/manager/factory.kt
+++ k-event-manager/src/commonMain/kotlin/com/fantamomo/kevent/manager/factory.kt
@@ -7,9 +7,9 @@ import com.fantamomo.kevent.manager.components.EventManagerComponent
 import com.fantamomo.kevent.manager.components.ExceptionHandler
 import com.fantamomo.kevent.manager.components.addIfAbsent
 
-fun EventManager(): EventManager = EventManager(ComponentSet.of())
+fun EventManager(defaultParameterInjection: Boolean = true): EventManager = EventManager(ComponentSet.of(), defaultParameterInjection)
 
-fun EventManager(components: EventManagerComponent<*>): EventManager {
+fun EventManager(components: EventManagerComponent<*>, defaultParameterInjection: Boolean = true): EventManager {
     val component = components.addIfAbsent(ExceptionHandler.Empty)
-    return DefaultEventManager(component)
+    return DefaultEventManager(component, defaultParameterInjection)
 }
