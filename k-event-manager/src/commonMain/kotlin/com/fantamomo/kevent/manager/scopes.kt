139db3463358043513033b288ff1547ccffcb76d
Fantamomo <git@fantamomo.com>
2025-08-10 06:32:29 +0200

    docs(HandlerEventScope): add KDoc for scope creation methods
    - Documented `HandlerEventScope` factory function with usage details.
    - Added KDoc for `HandlerEventScope.newScope` to explain child scope creation.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- k-event-manager/src/commonMain/kotlin/com/fantamomo/kevent/manager/scopes.kt
+++ k-event-manager/src/commonMain/kotlin/com/fantamomo/kevent/manager/scopes.kt
@@ -2,6 +2,24 @@ package com.fantamomo.kevent.manager
 
 import com.fantamomo.kevent.manager.internal.HandlerEventScopeImpl
 
+/**
+ * Creates a new instance of `HandlerEventScope` with a specified parent scope.
+ *
+ *
+ * @param parent The parent `HandlerEventScope` from which the new scope should inherit behavior.
+ * @return A new instance of `HandlerEventScope` that is tied to the specified parent.
+ *
+ * @author Fantamomo
+ * @since 1.0-SNAPSHOT
+ */
 fun HandlerEventScope(parent: HandlerEventScope): HandlerEventScope = HandlerEventScopeImpl(parent)
 
+/**
+ * Creates a new child scope derived from the current `HandlerEventScope`.
+ *
+ * @return A new instance of `HandlerEventScope` representing the child scope.
+ *
+ * @author Fantamomo
+ * @since 1.0-SNAPSHOT
+ */
 fun HandlerEventScope.newScope(): HandlerEventScope = HandlerEventScopeImpl(this)
