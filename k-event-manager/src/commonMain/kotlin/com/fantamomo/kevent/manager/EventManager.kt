3d50a811ef4000bd908bce06d308f2afeaa1f3ab
Fantamomo <git@fantamomo.com>
2025-08-10 06:29:00 +0200

    refactor(EventManager): simplify interface and delegate handler management to `HandlerEventScope`
    - Removed redundant `register` and `unregister` methods.
    - Updated `EventManager` to extend `HandlerEventScope` for streamlined functionality.
    - Simplified import statements by removing unused dependencies.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- k-event-manager/src/commonMain/kotlin/com/fantamomo/kevent/manager/EventManager.kt
+++ k-event-manager/src/commonMain/kotlin/com/fantamomo/kevent/manager/EventManager.kt
@@ -1,7 +1,6 @@
 package com.fantamomo.kevent.manager
 
-import com.fantamomo.kevent.*
-import kotlin.reflect.KClass
+import com.fantamomo.kevent.Dispatchable
 
 /**
  * Interface for managing event listeners and dispatching events in the event system.
@@ -12,21 +11,7 @@ import kotlin.reflect.KClass
  * @author Fantamomo
  * @since 1.0-SNAPSHOT
  */
-interface EventManager {
-    /**
-     * Registers an event listener with the event system.
-     *
-     * This method allows adding a class implementing the [Listener] interface
-     * to the event handling system. The event system will scan the listener
-     * for annotated methods (with [Register]) and register them as event handlers.
-     *
-     * @param listener The listener to be registered, containing methods annotated
-     *                 to handle specific events.
-     *
-     * @see Register
-     * @see Listener
-     */
-    fun register(listener: Listener)
+interface EventManager : HandlerEventScope {
 
     /**
      * Dispatches an event to all registered event listeners.
@@ -50,27 +35,4 @@ interface EventManager {
      * @param event The event instance to be dispatched to the appropriate listeners.
      */
     suspend fun dispatchSuspend(event: Dispatchable)
-
-    /**
-     * Registers an event-specific handler with its configuration.
-     *
-     * This method allows for registering a handler for a specific type of event. The handler
-     * will be invoked whenever an instance of the specified event class is dispatched. Additionally,
-     * a custom event configuration can be provided to modify event-specific behavior.
-     *
-     * @param E The type of event the handler will process.
-     * @param event The class of the event to register the handler for.
-     * @param configuration The configuration for the event handler. Defaults to the result of [EventConfiguration.default].
-     * @param handler The function to be invoked when the event is dispatched.
-     *
-     * @see createConfigurationScope
-     * @see EventConfigurationScope
-     */
-    fun <E : Dispatchable> register(
-        event: KClass<E>,
-        configuration: EventConfiguration<E> = EventConfiguration.default(),
-        handler: (E) -> Unit,
-    ): RegisteredLambdaHandler
-
-    fun unregister(listener: Listener)
 }
