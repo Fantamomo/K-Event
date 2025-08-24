cdf0b0bb051010412711978a6680f8ce3124dd75
Fantamomo <git@fantamomo.com>
2025-08-24 21:29:51 +0200

    feat(build): add kotlinx-datetime dependency
    - Included `kotlinx-datetime:0.7.1` for improved date and time handling.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- k-event-manager/build.gradle.kts
+++ k-event-manager/build.gradle.kts
@@ -16,6 +16,7 @@ kotlin {
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
                 implementation(project(":k-event-api"))
                 implementation("org.jetbrains.kotlin:kotlin-reflect")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.7.1")
             }
         }
         val commonTest by getting {
