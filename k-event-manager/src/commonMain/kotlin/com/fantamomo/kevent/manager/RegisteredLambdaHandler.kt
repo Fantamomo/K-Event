d4dbb4a33f10b89ae5058a1f2fe832d8853b85ea
Fantamomo <git@fantamomo.com>
2025-08-25 09:46:10 +0200

    docs(RegisteredLambdaHandler): add detailed functional interface documentation
    - Added comprehensive KDoc comments for `RegisteredLambdaHandler`.
    - Documented purpose, responsibilities, and usage as a callback mechanism.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- k-event-manager/src/commonMain/kotlin/com/fantamomo/kevent/manager/RegisteredLambdaHandler.kt
+++ k-event-manager/src/commonMain/kotlin/com/fantamomo/kevent/manager/RegisteredLambdaHandler.kt
@@ -1,5 +1,14 @@
 package com.fantamomo.kevent.manager
 
+/**
+ * Represents a functional interface for managing the lifecycle of a registered lambda handler.
+ *
+ * This interface is used as a callback mechanism to allow the caller
+ * to unregister a previously registered event-handling lambda.
+ *
+ * @author Fantamomo
+ * @since 1.0-SNAPSHOT
+ */
 fun interface RegisteredLambdaHandler {
     fun unregister()
 }
