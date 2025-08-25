b7dbafd801da14e3cdf4fe5784e1675719df2f10
Fantamomo <git@fantamomo.com>
2025-08-25 08:58:17 +0200

    feat(ExceptionHandler): add specialized exception handling methods
    - Added methods for handling specific method-related exceptions (e.g., visibility, parameters, accessibility).
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- k-event-manager/src/commonMain/kotlin/com/fantamomo/kevent/manager/components/ExceptionHandler.kt
+++ k-event-manager/src/commonMain/kotlin/com/fantamomo/kevent/manager/components/ExceptionHandler.kt
@@ -2,6 +2,8 @@ package com.fantamomo.kevent.manager.components
 
 import com.fantamomo.kevent.Listener
 import kotlin.reflect.KFunction
+import kotlin.reflect.KType
+import kotlin.reflect.KVisibility
 
 abstract class ExceptionHandler : EventManagerComponent<ExceptionHandler> {
     override val key: EventManagerComponent.Key<ExceptionHandler>
@@ -12,6 +14,20 @@ abstract class ExceptionHandler : EventManagerComponent<ExceptionHandler> {
     }
 
     abstract fun handle(exception: Throwable, listener: Listener?, methode: KFunction<*>?)
+
+    fun onMethodNotPublic(listener: Listener, method: KFunction<*>, visibility: KVisibility?) {}
+
+    fun onMethodHasNoParameters(listener: Listener, method: KFunction<*>) {}
+
+    fun onMethodHasNoDispatchableParameter(listener: Listener, method: KFunction<*>, type: KType) {}
+
+    fun onMethodNotAccessible(listener: Listener, method: KFunction<*>) {}
+
+    fun onMethodThrewUnexpectedException(listener: Listener, method: KFunction<*>, exception: Throwable) {}
+
+    fun onUnexpectedExceptionDuringRegistration(listener: Listener, method: KFunction<*>, exception: Throwable) {}
+
+    fun onMethodDidNotThrowConfiguredException(listener: Listener, method: KFunction<*>) {}
 
     object Empty : ExceptionHandler() {
         override fun handle(
