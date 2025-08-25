5dc074804e9f8f73baacc2f602b266806a8fe494
Fantamomo <git@fantamomo.com>
2025-08-25 09:43:43 +0200

    docs(EventManager): add detailed function documentation
    - Added comprehensive KDoc comments for `EventManager` functions.
    - Documented purpose, responsibilities, parameters, and return values of overloaded constructors.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- k-event-manager/src/commonMain/kotlin/com/fantamomo/kevent/manager/factory.kt
+++ k-event-manager/src/commonMain/kotlin/com/fantamomo/kevent/manager/factory.kt
@@ -7,8 +7,32 @@ import com.fantamomo.kevent.manager.components.EventManagerComponent
 import com.fantamomo.kevent.manager.components.ExceptionHandler
 import com.fantamomo.kevent.manager.components.addIfAbsent
 
+/**
+ * Creates a default instance of [EventManager] with a predefined empty component set.
+ *
+ * This function initializes an [EventManager] implementation using an empty [ComponentSet],
+ * enabling the management of event listeners and event dispatching without any pre-configured components.
+ *
+ * @return A default instance of [EventManager] with an empty component set.
+ *
+ * @author Fantamomo
+ * @since 1.0-SNAPSHOT
+ */
 fun EventManager(): EventManager = EventManager(ComponentSet.of())
 
+/**
+ * Constructs a new instance of an `EventManager` using the provided components.
+ * Ensures that the required [ExceptionHandler.Empty] component is present by adding
+ * it to the given components if it is not already included.
+ *
+ * @param components The initial set of components used to construct the `EventManager`.
+ *                   If `ExceptionHandler.Empty` is not present, it will be added.
+ * @return A newly constructed [EventManager] implementation with the provided and
+ *         required components.
+ *
+ * @author Fantamomo
+ * @since 1.0-SNAPSHOT
+ */
 fun EventManager(components: EventManagerComponent<*>): EventManager {
     val component = components.addIfAbsent(ExceptionHandler.Empty)
     return DefaultEventManager(component)
