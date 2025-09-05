1882c77a2a6aac015045d1e069ca2ee4044cfe24
Fantamomo <git@fantamomo.com>
2025-09-05 22:33:08 +0200

    feat(config): add `DispatchConfig` for advanced dispatch configuration management
    - Implemented `DispatchConfig` class to handle dispatch key-value pairs.
    - Added support for retrieving values using `get` and `getOrDefault` methods.
    - Enabled `contains` operator for checking keys and values.
    - Introduced `DispatchConfig.EMPTY` singleton for empty configurations.
    - Included direct creation of `DispatchConfig` using `DispatchConfigScope`.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

---
+++ k-event-manager/src/commonMain/kotlin/com/fantamomo/kevent/manager/config/DispatchConfig.kt
@@ -0,0 +1,19 @@
+package com.fantamomo.kevent.manager.config
+
+class DispatchConfig(private val data: Map<DispatchConfigKey<*>, Any?>) {
+
+    @Suppress("UNCHECKED_CAST")
+    operator fun <T> get(key: DispatchConfigKey<T>): T? = data[key] as? T?
+
+    fun <T> getOrDefault(key: DispatchConfigKey<T>) = get(key) ?: key.defaultValue
+
+    operator fun <T> contains(key: DispatchConfigKey<T>) = key in data
+
+    operator fun <T> contains(value: T) = value in data.values
+
+    companion object {
+        val EMPTY = DispatchConfig(emptyMap())
+
+        operator fun invoke(scope: DispatchConfigScope) = if (scope.data.isEmpty()) EMPTY else DispatchConfig(scope.data)
+    }
+}
