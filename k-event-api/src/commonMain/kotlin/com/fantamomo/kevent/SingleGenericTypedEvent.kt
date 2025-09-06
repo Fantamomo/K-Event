f2160d31f7e1b251666a226809810f23c1fd09e0
Fantamomo <git@fantamomo.com>
2025-09-06 12:09:26 +0200

    docs(event): refine KDoc for `SingleGenericTypedEvent` interface
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- k-event-api/src/commonMain/kotlin/com/fantamomo/kevent/SingleGenericTypedEvent.kt
+++ k-event-api/src/commonMain/kotlin/com/fantamomo/kevent/SingleGenericTypedEvent.kt
@@ -3,9 +3,9 @@ package com.fantamomo.kevent
 import kotlin.reflect.KClass
 
 /**
- * Specialized interface for events that contain only a single generic type parameter.
+ * Interface for events that contain a single generic type parameter.
  *
- * This is a convenience interface that extends [GenericTypedEvent], simplifying
+ * This is a convenience interface extending [GenericTypedEvent], simplifying
  * the declaration of events like:
  *
  * ```
@@ -14,10 +14,10 @@ import kotlin.reflect.KClass
  * }
  * ```
  *
- * The event system will automatically wrap this single type into a list for use
- * in matching logic via the inherited [extractGenericTypes] method.
+ * The event system automatically wraps this single type into a list for matching
+ * logic via the inherited [extractGenericTypes] method.
  *
- * Use this when your event only needs to expose one type parameter.
+ * Use this interface when your event only needs to expose one type parameter.
  *
  * @author Fantamomo
  * @since 1.0-SNAPSHOT
@@ -26,9 +26,8 @@ interface SingleGenericTypedEvent : GenericTypedEvent {
     /**
      * Returns the single runtime generic type associated with this event.
      *
-     * Should return the actual [KClass] instance representing the generic type `T`.
-     *
-     * It may return `Nothing::class` when the type is `null`.
+     * Should return the actual [KClass] representing the generic type `T`.
+     * If the type is unknown or `null`, `Nothing::class` can be returned.
      *
      * @return The generic type of this event instance.
      */
