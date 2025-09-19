9642d60d6ef8fad05e57dc119f5434595284761a
Fantamomo <git@fantamomo.com>
2025-09-19 17:24:04 +0200

    feat: implement `ListenerProcessorRegistry` for listener management
    - Added `ListenerProcessorRegistry` interface for managing event listeners and registries.
    - Introduced `Default` and `Empty` implementations for registry operations.
    - Incorporated concurrency handling with `ReentrantLock` for thread safety.
    - Enabled dynamic loading of listener registries using `ServiceLoader`.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

---
+++ k-event-manager/src/commonMain/kotlin/com/fantamomo/kevent/manager/components/ListenerProcessorRegistry.kt
@@ -0,0 +1,98 @@
+package com.fantamomo.kevent.manager.components
+
+import com.fantamomo.kevent.Listener
+import com.fantamomo.kevent.processor.EventHandlerRegistry
+import com.fantamomo.kevent.processor.GeneratedHandlerRegistry
+import com.fantamomo.kevent.processor.InternalProcessorApi
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
+import kotlin.reflect.KClass
+import kotlin.reflect.full.findAnnotation
+import kotlin.streams.asSequence
+
+@OptIn(InternalProcessorApi::class)
+sealed interface ListenerProcessorRegistry : EventManagerComponent<ListenerProcessorRegistry> {
+
+    override val key get() = Key
+
+    fun load()
+    fun getRegistry(listenerClass: KClass<out Listener>): EventHandlerRegistry?
+    fun init(listenerClass: KClass<out Listener>)
+    fun fullInit()
+
+    companion object Key : EventManagerComponent.Key<ListenerProcessorRegistry> {
+        override val clazz = ListenerProcessorRegistry::class
+
+        fun create(): ListenerProcessorRegistry = Default()
+    }
+
+    object Empty : ListenerProcessorRegistry {
+        override fun load() {}
+        override fun getRegistry(listenerClass: KClass<out Listener>): EventHandlerRegistry? = null
+        override fun init(listenerClass: KClass<out Listener>) {}
+        override fun fullInit() {}
+    }
+
+    class Default internal constructor() : ListenerProcessorRegistry {
+        private val serviceLoader = ServiceLoader.load(EventHandlerRegistry::class.java)
+        private val lock = ReentrantLock()
+
+        @Volatile
+        private var providedListeners: Map<KClass<out Listener>, KClass<out EventHandlerRegistry>>? = null
+
+        private val registries: MutableMap<KClass<out Listener>, EventHandlerRegistry> = ConcurrentHashMap()
+
+        @Volatile
+        private var finished = false
+
+        override fun load() {
+            if (finished) return
+            if (providedListeners != null) return
+
+            lock.withLock {
+                if (providedListeners != null) return
+
+                val providers = mutableMapOf<KClass<out Listener>, KClass<out EventHandlerRegistry>>()
+                serviceLoader.stream().asSequence().forEach { provider ->
+                    val clazz = provider.type().kotlin
+                    val annotation = clazz.findAnnotation<GeneratedHandlerRegistry>()
+                    if (annotation != null) {
+                        providers.putIfAbsent(annotation.value, clazz)
+                    }
+                }
+                providedListeners = providers
+            }
+        }
+
+        override fun getRegistry(listenerClass: KClass<out Listener>): EventHandlerRegistry? {
+            init(listenerClass)
+            return registries[listenerClass]
+        }
+
+        override fun init(listenerClass: KClass<out Listener>) {
+            load()
+            val localProvided = providedListeners ?: return
+            if (!localProvided.containsKey(listenerClass)) return
+            if (registries.containsKey(listenerClass)) return
+
+            lock.withLock {
+                if (!registries.containsKey(listenerClass)) {
+                    val registryClass = localProvided[listenerClass] ?: return
+                    val registry = registryClass.objectInstance
+                        ?: throw IllegalArgumentException("Class ${registryClass.qualifiedName} must be an object")
+                    registries[listenerClass] = registry
+                }
+                if (registries.size == localProvided.size) finished = true
+            }
+        }
+
+        override fun fullInit() {
+            load()
+            val localProvided = providedListeners ?: return
+            localProvided.keys.forEach { init(it) }
+            finished = true
+        }
+    }
+}
